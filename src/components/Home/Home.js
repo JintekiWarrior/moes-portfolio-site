842ab9044c573c6308d5f8313aa476cf3d6ecf01
JintekiWarrior <jintekiwarrior@gmail.com>
2021-08-02 13:28:29 -0400

    Add a projects component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,8 @@
 import React, { Fragment } from 'react'
 import AboutMe from './AboutMe/AboutMe'
 import Technologies from './Technologies/Technologies'
+import Projects from './Projects/Projects'
 import './Home.scss'
-import reactLogo from './images/react-logo.png'
-import htmlLogo from './images/html-logo.png'
-import cssLogo from './images/css-logo.png'
-import jsLogo from './images/js-logo.png'
-import mongoLogo from './images/mongodb-logo.png'
-import nodeJsLogo from './images/nodeJs-logo.png'
-
 
 const Home = () => {
   return (
@@ -30,10 +24,10 @@ const Home = () => {
           <h2>Technologies</h2>
           <Technologies />
         </div>
-        <h2 id="home-project-heading">Personal Projects</h2>
-        <a className="project-link" href="https://jintekiwarrior.github.io/createQuiz-client/">Quiz Crafter</a>
-        <a className="project-link" href="https://boo-leans.github.io/eCommerce-client/">Beats by Dwight</a>
-        <a className="project-link" href="https://jintekiwarrior.github.io/Story_Time_Frontend/">Story Time</a>
+        <div id="home-project-container">
+          <h2>Projects</h2>
+          <Projects />
+        </div>
       </div>
     </Fragment>
   )
---
+++ src/components/Home/Projects/Projects.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react'
+import './Projects.scss'
+import image1 from './images/beats-by-dwight.PNG'
+import image2 from './images/quiz-crafter.PNG'
+import image3 from './images/story-time.PNG'
+
+const Projects = () => {
+  return (
+    <Fragment>
+      <div id="project-image-container">
+        <a href="https://jintekiwarrior.github.io/Story_Time_Frontend/">
+          <img className="project-image" src={image3} />
+        </a>
+        <a href="https://jintekiwarrior.github.io/createQuiz-client/">
+          <img className="project-image" src={image2} />
+        </a>
+        <a href="https://boo-leans.github.io/eCommerce-client/">
+          <img className="project-image" src={image1} />
+        </a>
+      </div>
+    </Fragment>
+  )
+}
+
+export default Projects
