d287555f816d6904ce4a8372adf82bbbad077b1a
JintekiWarrior <jintekiwarrior@gmail.com>
2021-08-25 12:15:53 -0400

    change the projects layout

--- src/components/Home/Projects/Projects.js
+++ src/components/Home/Projects/Projects.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react'
 import './Projects.scss'
-import TextBox from './../../style-components/TextBox'
 // import image1 from './images/beats-by-dwight.PNG'
 // import image2 from './images/quiz-crafter.PNG'
 // import image3 from './images/story-time.PNG'
@@ -11,23 +10,19 @@ const Projects = () => {
   return (
     <Fragment>
       <h2 id="projects-heading">Projects</h2>
+      <div id="projects-container">
       {projectContent.map(project => (
-        <div id="project-container" key={project.id}>
-          <div id="content-container">
-            <TextBox id="project-text-box">
-              <h3>{project.name}</h3>
-              <p>{project.summary}</p>
-              <p>{project.frontTools}</p>
-              <p>{project.backTools}</p>
-            </TextBox>
-          </div>
+        <div id="project" key={project.id}>
           <div id="image-container">
             <a href={project.link}>
               <img id="project-image" src={project.image} alt={project.summary} />
             </a>
           </div>
+          <h3 id="project-heading">{project.name}</h3>
+          <p id="project-content">{project.summary}</p>
         </div>
       ))}
+      </div>
     </Fragment>
   )
 }
